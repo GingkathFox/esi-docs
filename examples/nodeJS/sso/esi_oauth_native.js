cda327084cca46e69d11e580e0aa0f483ea0c5d9
GingkathFox <gingkathfox@gmail.com>
2020-12-21 22:21:55 -0500

    attempt 1

---
+++ examples/nodeJS/sso/esi_oauth_native.js
@@ -0,0 +1,89 @@
+const Inquirer = require('inquirer')
+const Chalk = require('chalk')
+const crypto = require('crypto') // For the generation and hashing
+const {
+    createAuthURL,
+    sendTokenRequest,
+    params,
+    form_values,
+    validateToken
+} = require('./shared_flow')
+
+// Prepare questions
+const questions = [{
+        type: 'input',
+        name: 'clientID',
+        message: `Copy your SSO application's client ID and enter it here:`,
+        validate: (input) => {
+            if (input === '') {
+                return "Please enter a client ID."
+            }
+            return true
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirm',
+        message: `Press Enter to continue:`,
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'code',
+        message: `Copy the "code" query parameter and enter it here:`,
+        validate: (input) => {
+            if (input === '') {
+                return "Please enter the code."
+            }
+            return true
+        }
+    }
+]
+// Start program
+async function start() {
+    // create the challenge
+    const codeChallenge = await crypto.randomBytes(32).toString('base64')
+    // ^ Create the 32 byte string in base64...
+    const hashedCodeChallenge = await crypto.createHash('sha256')
+    await hashedCodeChallenge.update(codeChallenge)
+    // ^ ...and hash it
+
+    // now start the program
+    // Grab the client ID...
+    console.log(`This program will take you through an example OAuth 2.0 flow that you should be using if you are building a desktop or mobile application. Follow the prompts and enter the info asked for.`)
+    const questionOne = await Inquirer.prompt(questions[0])
+    const clientID = questionOne.clientID
+
+    // ...generate the URL...
+    console.log(`\nBecause this is a desktop/mobile application, you should use the PKCE protocol when contacting the EVE SSO. In this case, that means sending a base 64 encoded sha256 hashed 32 byte string called a code challenge. This 32 byte string should be ephemeral and never stored anywhere. The code challenge string generated for this program is ${Chalk.yellowBright(codeChallenge)} and the hashed code challenge is ${Chalk.yellowBright(hashedCodeChallenge.digest('base64'))}.`)
+    console.log(`Notice that the query parameter of the following URL will contain this code challenge.\n`)
+    await Inquirer.prompt(questions[1]) // no "const" since input isn't needed
+
+    // create the parameters...
+    let query = params
+    query.client_id = clientID
+    query.code_challenge = hashedCodeChallenge
+    console.log(`\nOpen the following link in your browser:\n`)
+    // ...display the url...
+    const authURL = await createAuthURL(query)
+    console.log(` ${authURL}\n`)
+    console.log(` Once you have logged in as a character you will get redirected to ${Chalk.yellowBright("https://localhost/callback/.")}`)
+
+    // ...prompt for the code...
+    const questionThree = await Inquirer.prompt(questions[2])
+    const code = questionThree.code
+
+    // ...and send the request...
+    let form = form_values
+    form.client_id = clientID
+    form.code = code
+    form.code_verifier = codeChallenge.replace("=", "")
+
+    const res = await sendTokenRequest(form)
+    const token = await validateToken(res)
+    console.log(`\nThe contents of the access token are: ${JSON.stringify(token, null, 2)}`)
+}
+start().catch(e => {
+    console.error(e.stack)
+    process.exit()
+})
---
+++ examples/nodeJS/sso/shared_flow.js
@@ -0,0 +1,88 @@
+const Axios = require("axios")
+const {
+    default: jwtVerify
+} = require('jose/jwt/verify')
+const {
+    default: parseJWK
+} = require('jose/jwk/parse')
+const {
+    parse
+} = require("path")
+
+module.exports = {
+    params: {
+        "response_type": "code",
+        "redirect_uri": "https://localhost/callback/",
+        "client_id": "ID",
+        "scope": "esi-characters.read_blueprints.v1",
+        "state": "unique-state",
+        "code_challenge": "CHALLENGE",
+        "code_challenge_method": "S256"
+    },
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Host": "login.eveonline.com",
+    },
+    form_values: {
+        "grant_type": "authorization_code",
+        "client_id": "client_id",
+        "code": "auth_code",
+        "code_verifier": "code_verifier"
+    },
+    createAuthURL(query) {
+        const baseURL = `https://login.eveonline.com/v2/oauth/authorize/`
+        let fullURL = `${baseURL}`
+        Object.keys(query).forEach(queryKey => {
+            // query params undefined or empty, or array of length 0
+            if (query[queryKey] === undefined || query[queryKey] === '') {
+                return
+            }
+            if (query[queryKey].length && query[queryKey].length === 0) {
+                return
+            }
+            fullURL += `&${queryKey}=${query[queryKey]}`
+        })
+        return encodeURI(fullURL).replace("&", "?") // encode the URI and replace the first & with a ?
+    },
+    async sendTokenRequest(body) {
+        try {
+            const res = await Axios.post("https://login.eveonline.com/v2/oauth/token", body, {
+                headers: module.exports.headers
+            })
+            return res.data
+        } catch (e) {
+            throw e // pass it to whatever called it
+        }
+    },
+    async validateToken(jwkToken) {
+        try {
+            const URLs = await module.exports.tokenInfo()
+            const jwkURL = URLs.jwks_uri
+
+            // Get the first key that ESI supports
+            const res = await Axios.get(jwkURL)
+            const keySet = res.data
+            const key = keySet.keys.filter(v => v.alg === "RS256")
+
+            // Validate the signature
+            const parsedJWK = await parseJWK(jwkToken, "RS256")
+            const esiPublicKey = await parseJWK(key)
+            const {
+                payload,
+                protectedHeader
+            } = await jwtVerify(jwt, publicKey, {
+                issuer: 'login.eveonline.com'
+            })
+        } catch (e) {
+            throw e
+        }
+    },
+    async tokenInfo() {
+        try {
+            const res = await Axios.get(`https://login.eveonline.com/.well-known/oauth-authorization-server`)
+            return res.data
+        } catch (e) {
+            throw e
+        }
+    }
+}
